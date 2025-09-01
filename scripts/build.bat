1754e363ed42c3195ee6fe9cdb601a350f7da36d
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-01 21:46:52 +0700

    fix: ensure PyInstaller command includes --onefile option for proper build

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,4 +2,4 @@
 rmdir /s /q dist
 rmdir /s /q build
 @echo off
-python -m PyInstaller main.py --contents-directory . --name "RFID Agent" --add-data="icon.ico;." --icon=icon.ico
+python -m PyInstaller main.py --contents-directory . --onefile --name "RFID Agent" --add-data="icon.ico;." --icon=icon.ico
