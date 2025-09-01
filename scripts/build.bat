3d81b6e4688f1166d6066a3ddb68a08391b271f3
quanghiep03198 <quanghiep03198@gmail.com>
2025-09-01 18:08:29 +0700

    feat: add icon file and update build script for PyInstaller packaging

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,12 +1,5 @@
-@echo off
-REM Lấy IPv4 hiện tại của máy tính
-for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /c:"IPv4"') do (
-    set "ip=%%a"
-    goto :run
-)
 
-:run
-REM Loại bỏ khoảng trắng đầu dòng
-set "ip=%ip:~1%"
-REM Chạy gunicorn với host là IPv4 vừa lấy và port 3198
-gunicorn --bind %ip%:3198 app:app
+rmdir /s /q dist
+rmdir /s /q build
+@echo off
+python -m PyInstaller main.py --contents-directory . --name "RFID Agent" --add-data="icon.ico;." --icon=icon.ico
